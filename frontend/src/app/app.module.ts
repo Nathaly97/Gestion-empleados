e73234caec8d3667dfcbccfbe34a88a2e4b30f56
Angie N. Gonzalez <nthalygc1997@gmail.com>
2025-03-11 21:28:57 -0500

    ok

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,43 +1,22 @@
-    import { NgModule } from '@angular/core';
-
-import { BrowserModule } from '@angular/platform-browser';
-
-import {FormsModule} from '@angular/forms';
-
-import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
-
 import { EmpleadosComponent } from './components/empleados/empleados.component';
 
 @NgModule({
-
-declarations: [
-
-AppComponent,
-
-EmpleadosComponent
-
-],
-
-imports: [
-
-BrowserModule,
-
-FormsModule,
-
-AppRoutingModule,
-
-HttpClientModule
-
-],
-
-providers: [ ],
-
-bootstrap: [AppComponent]
-
+  declarations: [
+    AppComponent,
+    EmpleadosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
 })
-
 export class AppModule { }
--- frontend/src/app/components/empleados/empleados.component.ts
+++ frontend/src/app/components/empleados/empleados.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-empleados',
-  standalone: true,
-  imports: [],
   templateUrl: './empleados.component.html',
   styleUrl: './empleados.component.css'
 })
